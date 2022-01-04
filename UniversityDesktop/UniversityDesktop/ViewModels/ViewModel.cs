28e3e76025f4adb9e114238f07e5d9da085fe349
newmersedez <trishkk2001@gmail.com>
2022-01-04 16:48:32 +0300

    client recv json from server, started user auth

--- UniversityDesktop/UniversityDesktop/ViewModels/ViewModel.cs
+++ UniversityDesktop/UniversityDesktop/ViewModels/ViewModel.cs
@@ -1,5 +1,8 @@
-﻿using System.Net;
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
 using System.Net.Sockets;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Windows;
 using System.Windows.Input;
@@ -12,24 +15,34 @@ namespace UniversityDesktop.ViewModels
     {
         #region Variables
 
+        // Pages
         private readonly string _eventsPagePath = "../Pages/EventsPage.xaml";
         private readonly string _examTimetablePagePath = "../Pages/ExamTimetablePage.xaml";
         private readonly string _lessonTimetablePagePath = "../Pages/LessonTimetablePage.xaml";
         private readonly string _MarksPagePath = "../Pages/MarksPage.xaml";
 
+        // Student info
         private string _currentFramePage;
-        private string _studentName = "Дмитрий";
-        private string _studentLastname = "Тришин";
-        private string _studentPatronymic = "Александрович";
-        private string _studentGroup = "М8О-311Б-19";
-        private string _specialtyNumber = "02.03.02";
-        private string _specialtyName = "Фундаментальная информатика и информационные технологии";
+        private string _studentName;
+        private string _studentLastname;
+        private string _studentPatronymic;
+        private string _studentGroup;
+        private string _specialtyNumber;
+        private string _specialtyName;
         
+        // Login window info
+        private string _studentLogin;
+        private string _studentPassword;
+        private string _errorMsg;
+
+        // Commands
         private ICommand _eventsButtonCommand;
         private ICommand _examTimetableButtonCommand;
         private ICommand _lessonTimetableButtonCommand;
         private ICommand _marksButtonCommand;
+        private ICommand _loginButtonCommand;
 
+        // Connection info
         private const string _host = "localhost";
         private const int _port = 5430;
         private byte[] _buffer = new byte[1024];
@@ -44,7 +57,7 @@ namespace UniversityDesktop.ViewModels
                 _currentFramePage;
             set
             {
-                _currentFramePage = value; 
+                _currentFramePage = value;
                 RaisePropertyChanged(nameof(CurrentFramePage));
             }
         }
@@ -56,7 +69,7 @@ namespace UniversityDesktop.ViewModels
             set =>
                 _studentName = value;
         }
-        
+
         public string StudentLastname
         {
             get =>
@@ -64,7 +77,7 @@ namespace UniversityDesktop.ViewModels
             set =>
                 _studentLastname = value;
         }
-        
+
         public string StudentPatronymic
         {
             get =>
@@ -72,7 +85,7 @@ namespace UniversityDesktop.ViewModels
             set =>
                 _studentPatronymic = value;
         }
-        
+
         public string StudentGroup
         {
             get =>
@@ -80,7 +93,7 @@ namespace UniversityDesktop.ViewModels
             set =>
                 _studentGroup = value;
         }
-        
+
         public string SpecialtyNumber
         {
             get =>
@@ -88,7 +101,7 @@ namespace UniversityDesktop.ViewModels
             set =>
                 _specialtyNumber = value;
         }
-        
+
         public string SpecialtyName
         {
             get =>
@@ -97,6 +110,42 @@ namespace UniversityDesktop.ViewModels
                 _specialtyName = value;
         }
 
+        public string StudentLogin
+        {
+            get =>
+                _studentLogin;
+            set
+            {
+                _studentLogin = value;
+                RaisePropertyChanged(nameof(Login));
+            }
+        }
+
+        public string StudentPassword
+        {
+            get =>
+                _studentPassword;
+            set
+            {
+                _studentPassword = value;
+                RaisePropertyChanged(nameof(Login));
+            }
+        }
+        
+        public string ErrorMsg
+        {
+            get =>
+                _errorMsg;
+            set
+            {
+                _errorMsg = value;
+                RaisePropertyChanged(nameof(Login));
+            }
+        }
+
+        public ICommand LoginButtonCommand =>
+            _loginButtonCommand = new RelayCommand(_ => Login());
+        
         public ICommand EventsButtonCommand =>
             _eventsButtonCommand = new RelayCommand(_ => GetEvents());
 
@@ -109,10 +158,15 @@ namespace UniversityDesktop.ViewModels
         public ICommand MarksButtonCommand =>
             _marksButtonCommand = new RelayCommand(_ => GetMarks());
         
-
         #endregion
 
         #region Functions
+
+        private void Login()
+        {
+            RegistrationWindow regWindow = new RegistrationWindow();
+            regWindow.Show();
+        }
 
         private void GetEvents()
         {
@@ -122,9 +176,19 @@ namespace UniversityDesktop.ViewModels
                 _socket.Connect(IPAddress.Loopback, _port);
                 _buffer = Encoding.ASCII.GetBytes("Events");
                 _socket.Send(_buffer);
+
+                byte[] recvBuffer = new byte[1024];
+                int recvNumber = _socket.Receive(recvBuffer);
+                char[] chars = new char[recvNumber];
+                System.Text.Decoder d = System.Text.Encoding.UTF8.GetDecoder();
+                int charLen = d.GetChars(recvBuffer, 0, recvNumber, chars, 0);
+                string recv = new string(chars);
+
                 _socket.Shutdown(SocketShutdown.Both);
                 _socket.Close();
+                
                 CurrentFramePage = _eventsPagePath;
+                MessageBox.Show(recv, "RECIEVED");
             }
             catch (SocketException)
             {
@@ -138,11 +202,21 @@ namespace UniversityDesktop.ViewModels
             {
                 Socket _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
                 _socket.Connect(IPAddress.Loopback, _port);
-                _buffer = Encoding.ASCII.GetBytes("Exams");
+                _buffer = Encoding.ASCII.GetBytes("Events");
                 _socket.Send(_buffer);
+
+                byte[] recvBuffer = new byte[1024];
+                int recvNumber = _socket.Receive(recvBuffer);
+                char[] chars = new char[recvNumber];
+                System.Text.Decoder d = System.Text.Encoding.UTF8.GetDecoder();
+                int charLen = d.GetChars(recvBuffer, 0, recvNumber, chars, 0);
+                string recv = new string(chars);
+
                 _socket.Shutdown(SocketShutdown.Both);
                 _socket.Close();
+                
                 CurrentFramePage = _examTimetablePagePath;
+                MessageBox.Show(recv, "RECIEVED");
             }
             catch (SocketException)
             {
@@ -156,11 +230,21 @@ namespace UniversityDesktop.ViewModels
             {
                 Socket _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
                 _socket.Connect(IPAddress.Loopback, _port);
-                _buffer = Encoding.ASCII.GetBytes("Lessons");
+                _buffer = Encoding.ASCII.GetBytes("Events");
                 _socket.Send(_buffer);
+
+                byte[] recvBuffer = new byte[1024];
+                int recvNumber = _socket.Receive(recvBuffer);
+                char[] chars = new char[recvNumber];
+                System.Text.Decoder d = System.Text.Encoding.UTF8.GetDecoder();
+                int charLen = d.GetChars(recvBuffer, 0, recvNumber, chars, 0);
+                string recv = new string(chars);
+
                 _socket.Shutdown(SocketShutdown.Both);
                 _socket.Close();
+                
                 CurrentFramePage = _lessonTimetablePagePath;
+                MessageBox.Show(recv, "RECIEVED");
             }
             catch (SocketException)
             {
@@ -174,11 +258,21 @@ namespace UniversityDesktop.ViewModels
             {
                 Socket _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
                 _socket.Connect(IPAddress.Loopback, _port);
-                _buffer = Encoding.ASCII.GetBytes("Marks");
+                _buffer = Encoding.ASCII.GetBytes("Events");
                 _socket.Send(_buffer);
+
+                byte[] recvBuffer = new byte[1024];
+                int recvNumber = _socket.Receive(recvBuffer);
+                char[] chars = new char[recvNumber];
+                System.Text.Decoder d = System.Text.Encoding.UTF8.GetDecoder();
+                int charLen = d.GetChars(recvBuffer, 0, recvNumber, chars, 0);
+                string recv = new string(chars);
+
                 _socket.Shutdown(SocketShutdown.Both);
                 _socket.Close();
+                
                 CurrentFramePage = _MarksPagePath;
+                MessageBox.Show(recv, "RECIEVED");
             }
             catch (SocketException)
             {
---
+++ UniversityDesktop/UniversityDesktop/Windows/RegistrationWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace UniversityDesktop
+{
+    public partial class RegistrationWindow : Window
+    {
+        public RegistrationWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
